b51b16de2a4949facdd012d2df423c4d284ce897
Saulo Weikert BIcalho <sbicalho86@gmail.com>
2018-09-23 23:12:28 -0300

    funcao remocao implementada

--- usuarios/main.py
+++ usuarios/main.py
@@ -6,10 +6,14 @@ TOKEN ='12345'
 app = Flask(__name__)
 
 def cadastrar()
-	url = '/usuario/registrar{0}/sendMessage'.format(TOKEN)
+	url = '/usuario/registrar{0}/'.format(TOKEN)
 	data = {'apelido', 'nome', 'bio', 'id_usuario'}
 	response = requests.post(url, data = data)
 
+def remover()
+	url = '/usuario/abandonar/@id_usuario{0}/'.format(TOKEN)
+	data = {id_usuario'}
+	response = requests.delete(url, data = data)
 
 
 if __name__ == '__main__':
@@ -20,3 +24,4 @@ if __name__ == '__main__':
 	id_usuario  = input('Entre com a id_usuario: ')
 
 	cadastrar()
+	remover(id_usuario)
