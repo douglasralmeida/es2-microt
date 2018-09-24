874cad69d1c54c1ceedaac36606bbe3273007238
Saulo Weikert BIcalho <sbicalho86@gmail.com>
2018-09-23 23:00:25 -0300

    falta o usuario digitar os dados antes da requisicao

--- usuarios/main.py
+++ usuarios/main.py
@@ -5,12 +5,12 @@ TOKEN ='12345'
 
 app = Flask(__name__)
 
-def sendMessage()
+def send_message()
 	url = '/usuario/registrar{0}/sendMessage'.format(TOKEN)
 	data = {'apelido', 'nome', 'bio', 'id_usuario'}
 	response = requests.post(url, data = data)
-	
+
 
 
 if __name__ == '__main__':
-  app.run(debug=True, use_reloader=True)
+  send_message()
