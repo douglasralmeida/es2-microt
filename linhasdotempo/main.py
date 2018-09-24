d6adaef3e95cc2d91d64da56516a9c3410e65bf1
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2018-09-24 03:09:59 -0300

    Teste

--- linhasdotempo/main.py
+++ linhasdotempo/main.py
@@ -9,5 +9,9 @@ app = Flask(__name__)
 def home():
   return 'microT Timeline Microservice'
 
+@app.route('/a')
+def a():
+  return 'teste'
+
 if __name__ == '__main__':
   app.run(debug=True, use_reloader=True)
