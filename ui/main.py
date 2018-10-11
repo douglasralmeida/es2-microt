5d6d5ed009000e9e0ffbcf2f4ba56bad0169442b
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2018-10-11 02:34:06 -0300

    Preparando arquivos para Flask

--- ui/main.py
+++ ui/main.py
@@ -1,12 +1,16 @@
 ## Módulo Interface do Usuário do microT
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
   return 'microT UI Microservice'
+
+@app.route('/login')
+def login():
+return render_template('login.html')
 
 if __name__ == '__main__':
   app.run(debug=True, use_reloader=True)
