7971f5d76f2b2385cedd0a5cd164a4227e19ceb5
Douglas Rodrigues de Almeida <douglasralmeida@live.com>
2018-10-23 15:24:12 -0300

    Adiciona suporte a CORS no módulo Mensagens

--- mensagens/main.py
+++ mensagens/main.py
@@ -12,6 +12,13 @@ SQL_MSG_USUARIO = 'SELECT usuario, conteudo FROM mtmensagens.mensagens WHERE usu
 app = Flask(__name__)
 conn = db.conectar()
 
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
+
 @app.route('/')
 def home():
   return 'microT Messages Microservice'
